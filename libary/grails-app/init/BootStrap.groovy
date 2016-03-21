261e5a6ff063843f584bb0ec38bacded490c8a40
student <student@b4028295.localdomain>
2016-03-21 13:32:06 +0000

    implemented scaffolding controller and bootstrapping to the library class

--- libary/grails-app/init/BootStrap.groovy
+++ libary/grails-app/init/BootStrap.groovy
@@ -25,6 +25,29 @@ class BootStrap {
 				password: 'password',
 				telephone: '0114297456').save()
 
+			
+
+			def libary1= new Libary
+				(location: 'Sheffield',
+				openingHours: '9-5',
+				book: 'Book1',
+				student: 'Barry Allen',
+				librarian: 'Jessica Jones').save()
+
+			def libary2= new Libary
+				(location: 'Manchester',
+				openingHours: '9-5',
+				book: 'Book2',
+				student: 'Oliver Queen',
+				librarian: 'James write').save()
+
+			def libary3= new Libary
+				(location: 'Liverpool',
+				openingHours: '9-5',
+				book: 'Book3',
+				student: 'Laurel Lance',
+				librarian: 'bob tank').save()
+
     }
     def destroy = {
     }

6e7ea9ff43943120662df140df0ed2bd8f755d03
student <student@b4028295.localdomain>
2016-03-24 15:19:19 +0000

    Relationships of library can have one or more librarians and libary can have one pr more books

--- libary/grails-app/init/BootStrap.groovy
+++ libary/grails-app/init/BootStrap.groovy
@@ -25,50 +25,141 @@ class BootStrap {
 				password: 'password',
 				telephone: '0114297456').save()
 
+			def librarian4= new Librarian
+				(name:'Thomas Gings',
+				email: 'TGlibrarian@public1.com',
+				userName: 'Tomgin123',
+				password: 'password',
+				telephone: '01142653198').save()
+
+			def librarian5= new Librarian
+				(name:'Sophie Cotton',
+				email: 'SClibrarian@public1.com',
+				userName: 'Sopcot123',
+				password: 'password',
+				telephone: '01142951233').save()
+
+			def librarian6= new Librarian
+				(name:'Tina Dents',
+				email: 'TDlibrarian@public1.com',
+				userName: 'Tinden123',
+				password: 'password',
+				telephone: '01142097511').save()
+
+			def librarian7= new Librarian
+				(name:'Ashley Moore',
+				email: 'AMlibrarian@public1.com',
+				userName: 'Ashmoo123',
+				password: 'password',
+				telephone: '01142987776').save()
+
+			def librarian8= new Librarian
+				(name:'Scott Wilson',
+				email: 'SWlibrarian@public1.com',
+				userName: 'Scowil123',
+				password: 'password',
+				telephone: '01142123409').save()
+
+			def librarian9= new Librarian
+				(name:'Connor Prescott',
+				email: 'CPlibrarian@public1.com',
+				userName: 'Conpresl123',
+				password: 'password',
+				telephone: '01142975312').save()
+
+
+
+
 			
 
 			def libary1= new Libary
 				(location: 'Sheffield',
 				openingHours: '9-5',
 				book: 'sci fi',
-				student: 'Barry Allen',
-				librarian: 'Jessica Jones').save()
+				student: 'Barry Allen').save()
 
 			def libary2= new Libary
 				(location: 'Manchester',
 				openingHours: '9-5',
 				book: 'fantasy',
-				student: 'Oliver Queen',
-				librarian: 'James write').save()
+				student: 'Oliver Queen').save()
 
 			def libary3= new Libary
 				(location: 'Liverpool',
 				openingHours: '9-5',
 				book: 'romantic',
-				student: 'Wally West',
-				librarian: 'bob tank').save()
+				student: 'Wally West').save()
+
 
 
 			def book1= new Book
-				(title: 'Space man',
-				author: 'Robbie Amelle',
+				(title: 'To Kill a Mockingbird',
+				author: 'Harper Lee',
 				isbn: '1541098531',
 				dateBorrowed: '21/03/2016',
 				returnDate: '04/04/2016',
 				student: 'Barry Allen').save()
 
 			def book2= new Book
-				(title: 'Open world',
-				author: 'Feleicty Smoke',
+				(title: 'Dune',
+				author: 'Frank Herbert',
 				isbn: '9712308523',
 				dateBorrowed: '21/03/2016',
 				returnDate: '04/04/2016',
 				student: 'Oliver Queen').save()
 
 			def book3= new Book
-				(title: 'The lonley one',
-				author: 'Harrison Wells',
+				(title: 'A Game Of Thrones',
+				author: 'George rr Martin',
 				isbn: '3014297412',
+				dateBorrowed: '21/03/2016',
+				returnDate: '04/04/2016',
+				student: 'Wally West').save()
+
+			def book4= new Book
+				(title: 'Harry Potter',
+				author: 'Jk Rowling',
+				isbn: '9010951238',
+				dateBorrowed: '21/03/2016',
+				returnDate: '04/04/2016',
+				student: 'Wally West').save()
+
+			def book5= new Book
+				(title: 'Hunger Games',
+				author: 'Suzanna Collins',
+				isbn: '0098512398',
+				dateBorrowed: '21/03/2016',
+				returnDate: '04/04/2016',
+				student: 'Wally West').save()
+
+			def book6= new Book
+				(title: '50 Shades of Grey',
+				author: 'E.L James',
+				isbn: '7765120945',
+				dateBorrowed: '21/03/2016',
+				returnDate: '04/04/2016',
+				student: 'Wally West').save()
+
+			def book7= new Book
+				(title: 'The Hobbit',
+				author: 'J.R.r. Tolkien',
+				isbn: '9612095323',
+				dateBorrowed: '21/03/2016',
+				returnDate: '04/04/2016',
+				student: 'Wally West').save()
+
+			def book8= new Book
+				(title: 'Stormbreaker',
+				author: 'Anthony Horowitz',
+				isbn: '7120985312',
+				dateBorrowed: '21/03/2016',
+				returnDate: '04/04/2016',
+				student: 'Wally West').save()
+
+			def book9= new Book
+				(title: 'Jurrassic Park',
+				author: 'Michael Crichton',
+				isbn: '1098765432',
 				dateBorrowed: '21/03/2016',
 				returnDate: '04/04/2016',
 				student: 'Wally West').save()
@@ -92,6 +183,43 @@ class BootStrap {
 				studentId: 'B6098755',
 				course: 'Motor Mechanics').save()
 
+			def student4= new Student
+				(name: 'Borris Johnson',
+				email: 'BorrisJ@localuni.com',
+				studentId: 'B4026156',
+				course: 'Polotics').save()
+
+			def student5= new Student
+				(name: 'Susan Pots',
+				email: 'SusanP@localuni.com',
+				studentId: 'B9063123',
+				course: 'Web Design').save()
+
+			def student6= new Student
+				(name: 'Chloe Fer',
+				email: 'ChloeF@localuni.com',
+				studentId: 'B1236540',
+				course: 'Dance').save()
+
+			def student7= new Student
+				(name: 'Daniel Stacy',
+				email: 'DanielS@localuni.com',
+				studentId: 'B7610956',
+				course: 'Interactive Media').save()
+
+			def student8= new Student
+				(name: 'Amy Harris',
+				email: 'AmyH@localuni.com',
+				studentId: 'B0964531',
+				course: 'Mathmatics').save()
+
+			def student9= new Student
+				(name: 'Paul Garner',
+				email: 'AmyH@localuni.com',
+				studentId: 'B0964531',
+				course: 'English').save()
+
+
 
 			def course1= new Course
 				(title: 'Forensic science',
@@ -113,8 +241,68 @@ class BootStrap {
 				tutor: 'David Ramsey',
 				department: 'Mechanics',
 				description: '''Here we teach Motor Mechanics. Here 					we teach Motor Mechanics. Here we teach Motor 					Mechanics. ''').save()
-				
 
+			def course4= new Course
+				(title: 'Polotics',
+				code: 'Po123',
+				tutor: 'Sarah Connor',
+				department: 'Communications',
+				description: '''Here we teach Polotics. Here 					we teach Polotics. Here we teach Polotics. ''').save()
+
+			def course5= new Course
+				(title: 'Web Design',
+				code: 'W123',
+				tutor: 'Jake Hart',
+				department: 'Computing',
+				description: '''Here we teach Web Design. Here 					we teach Web Design. Here we teach Web Design. 					''').save()
+
+			def course6= new Course
+				(title: 'Dance',
+				code: 'D123',
+				tutor: 'Melissa Hert',
+				department: 'Sports',
+				description: '''Here we teach Dance. Here 					we teach Dance. Here we teach Dance. 					''').save()
+				
+			def course7= new Course
+				(title: 'Interactive Media',
+				code: 'In123',
+				tutor: 'Grayhem Cray',
+				department: 'Computing',
+				description: '''Here we teach Interactive Media. Here 					we teach Interactive Media. Here we teach Interactive 					Media.''').save()
+
+			def course8= new Course
+				(title: 'Mathmatics',
+				code: 'Ma123',
+				tutor: 'Max Yurnse',
+				department: 'Core',
+				description: '''Here we teach Mathmatics. Here 					we teach Mathmatics. Here we teach 					Mathmatics.''').save()
+
+			def course9= new Course
+				(title: 'English',
+				code: 'En123',
+				tutor: 'Fern Atkin',
+				department: 'Core',
+				description: '''Here we teach English. Here 					we teach English. Here we teach 				English.''').save()
+
+			libary1.addToLibrarians(librarian1)
+			libary1.addToLibrarians(librarian2)
+			libary1.addToLibrarians(librarian3)
+			libary2.addToLibrarians(librarian4)
+			libary2.addToLibrarians(librarian5)
+			libary2.addToLibrarians(librarian6)
+			libary3.addToLibrarians(librarian7)
+			libary3.addToLibrarians(librarian8)
+			libary3.addToLibrarians(librarian9)
+	
+			libary1.addToBooks(book1)
+			libary1.addToBooks(book2)
+			libary1.addToBooks(book3)
+			libary2.addToBooks(book4)
+			libary2.addToBooks(book5)
+			libary2.addToBooks(book6)
+			libary3.addToBooks(book7)
+			libary3.addToBooks(book8)
+			libary3.addToBooks(book9)
 
     }
     def destroy = {
